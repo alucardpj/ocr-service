a64256799756da055ccfe1dd923a67e187e42742
alucardpj <alucardpj@users.noreply.github.com>
2023-06-15 02:51:14 +0000

    ✅ Ready to clone and code.

--- .github/rename_project.sh
+++ .github/rename_project.sh
@@ -16,10 +16,10 @@ echo "Description: $description";
 
 echo "Renaming project..."
 
-original_author="author_name"
-original_name="project_name"
-original_urlname="project_urlname"
-original_description="project_description"
+original_author="alucardpj"
+original_name="ocr_service"
+original_urlname="ocr-service"
+original_description="Awesome ocr_service created by alucardpj"
 # for filename in $(find . -name "*.*") 
 for filename in $(git ls-files) 
 do
@@ -30,7 +30,7 @@ do
     echo "Renamed $filename"
 done
 
-mv project_name $name
+mv ocr_service $name
 
 # This command runs only once on GHA!
 rm -rf .github/template.yml
--- ocr_service/app.py
+++ ocr_service/app.py
@@ -23,18 +23,18 @@ def read(*paths, **kwargs):
 
 
 description = """
-project_name API helps you do awesome stuff. 🚀
+ocr_service API helps you do awesome stuff. 🚀
 """
 
 app = FastAPI(
-    title="project_name",
+    title="ocr_service",
     description=description,
     version=read("VERSION"),
-    terms_of_service="http://project_name.com/terms/",
+    terms_of_service="http://ocr_service.com/terms/",
     contact={
-        "name": "author_name",
-        "url": "http://project_name.com/contact/",
-        "email": "author_name@project_name.com",
+        "name": "alucardpj",
+        "url": "http://ocr_service.com/contact/",
+        "email": "alucardpj@ocr_service.com",
     },
     license_info={
         "name": "The Unlicense",
